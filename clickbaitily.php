4fd9baaf0f188876d78df0ff3c2e4c30c714b1ad
Yeobie <konoha.shoujo@gmail.com>
2022-09-11 20:26:35 +0200

    added jQuery and Bootstrap JS

--- clickbaitily.php
+++ clickbaitily.php
@@ -56,5 +56,12 @@ include("functionsily.php");
         ?>
         
         </div>
+
+         <!-- jQuery -->
+        <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+        
+        <!-- Bootstrap JS -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        
     </body>
 </html>
